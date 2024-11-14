da3f73f6ba3f5be6424eae42447ccb5ec4dae948
Nikolay <75427338+korhenon@users.noreply.github.com>
2024-11-14 18:32:41 +0300

    Add requirements.txt

--- main.py
+++ main.py
@@ -9,5 +9,4 @@ def main():
     def iterfile():
         with open("cats.mp4", mode="rb") as file_like:
             yield from file_like
-
     return StreamingResponse(iterfile(), media_type="video/mp4")
